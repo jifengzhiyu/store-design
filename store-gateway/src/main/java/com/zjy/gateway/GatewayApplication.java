11939a59f4edc615700f05e4326d02b097ecf933
jifengzhiyu <2716105592@qq.com>
2023-05-29 17:01:37 +0800

    完成网关搭建

---
+++ store-gateway/src/main/java/com/zjy/gateway/GatewayApplication.java
@@ -0,0 +1,20 @@
+package com.zjy.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+  * @Author kaixin
+  * @Description 网关服务启动类
+  * @Date 00:42 2023/5/29
+  */
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class,args);
+    }
+}
