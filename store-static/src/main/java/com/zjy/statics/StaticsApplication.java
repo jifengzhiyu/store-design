8c622b9d3a48f61de5cc920fad52e01df60f1da9
jifengzhiyu <2716105592@qq.com>
2023-06-02 23:14:16 +0800

    静态资源转发配置

---
+++ store-static/src/main/java/com/zjy/statics/StaticsApplication.java
@@ -0,0 +1,19 @@
+package com.zjy.statics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author kaixin
+ * @ClassName StaticsApplication
+ * @Description 静态资源
+ * @Version 1.0
+ * @since 2023/6/2 21:33
+ */
+
+@SpringBootApplication
+public class StaticsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StaticsApplication.class, args);
+    }
+}
